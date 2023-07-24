7ee029f7213ff8966fa723b1cd269f66137a6072
FraBif <francescobif@hotmail.it>
2023-07-24 16:03:14 +0200

    populated the index blade with for each not visualizing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        @foreach ($movies as $movie)
+            <div style="border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;">
+                <h2>{{ $movie->title }}</h2>
+                <p>Anno: {{ $movie->nationality }}</p>
+                <p>Genere: {{ $movie->date }}</p>
+            </div>
+        @endforeach
+    </div>
+</body>
+</html>
